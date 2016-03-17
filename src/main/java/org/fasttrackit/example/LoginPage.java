159b0e27919cb76c023fc9f8e3be6d6cfdc7fdbc
rlidia <lidia.ratiu@gmail.com>
2016-03-17 21:08:17 +0200

    selenium test cases for Login page

--- src/main/java/org/fasttrackit/example/LoginPage.java
+++ src/main/java/org/fasttrackit/example/LoginPage.java
@@ -1,18 +1,26 @@
 package org.fasttrackit.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class LoginPage {
 
     @FindBy(how=How.ID,using="email")
     private WebElement emailField;
 
-    @FindBy(how=How.ID,using="password")
+    @FindBy(how=How.NAME,using="password")
     private WebElement passwordField;
 
     @FindBy(how=How.CLASS_NAME,using="login-btn")
     private WebElement loginBtn;
+
+    @FindBy(how=How.CLASS_NAME,using="error-msg")
+    private WebElement warningMsj;
 
     public void enterEmail(String email){
         System.out.println("Email :"+email);
@@ -28,10 +36,14 @@ public class LoginPage {
     }
 
     public void doLogin(String userName, String passWord) {
-        enterEmail(userName);
-        enterPassword(passWord);
-        clickLogin();
+        emailField.sendKeys(userName);
+        passwordField.sendKeys(passWord);
+        loginBtn.click();
 
     }
 
+    public  void assertThatErrorIs(String message) {
+        System.out.println(warningMsj.getText());
+        assertThat(warningMsj.getText(), is(message));
+    }
 }
