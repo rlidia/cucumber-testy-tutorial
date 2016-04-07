20c9b57bb83b4fb891ad0039d196b490787c3975
rlidia <lidia.ratiu@gmail.com>
2016-04-07 19:47:00 +0300

    DataProvider a new method invalidLoginTest that replaced 3 test

--- src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
+++ src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test
 public class FirstLoginTest extends TestBase {
 
-    private LoginView loginPage=new LoginView();
+    private LoginView loginPage = new LoginView();
 
 //ctrl+q see documentation to a method
 
@@ -23,42 +24,36 @@ public class FirstLoginTest extends TestBase {
         loginPage.doLogin("eu@fast.com", "eu.pass");
 
         Utils.sleep(2000);
-        WebLink logoutLink=new WebLink().setText("Logout");
+        WebLink logoutLink = new WebLink().setText("Logout");
         logoutLink.assertClick();
 
+    }
+
+    @Test(dataProvider = "invalidLogin")
+    public void invalidLoginTest(String email,String password, String messageResult) {
+        System.out.println("run this invalid test"+email+" - "+password+" - "+ messageResult);
+
+        openUrl();
+        loginPage.doLogin(email, password);
+        loginPage.assertThatErrorIs(messageResult);
     }
 
     private void openUrl() {
         driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
     }
 
-    @Test
-    public void failedLoginTest() {
-        openUrl();
-        loginPage.doLogin("eu@fast.com", "eu.passERROR");
-        loginPage.assertThatErrorIs("Please enter your password!");
+    @DataProvider
+    public static Object[][] invalidLogin() {
+
+        return new Object[][]{
+                {"eu@fast.com", "eu.passERROR", "Invalid user or password!"},
+                {"eu@fast.com", "", "Please enter your password!"},
+                {"", "eu@fast.com", "Please enter your email!"},
+                {"", "", "Please enter your email!"}
+        };
     }
 
-    @Test
-    public void withoutPasswordLoginTest() {
-        openUrl();
-        loginPage.doLogin("eu@fast.com", "");
-        loginPage.assertThatErrorIs("Please enter your password!");
-    }
 
-    @Test
-    public void withoutEmailLoginTest() {
-        openUrl();
-        loginPage.doLogin("", "eu@fast.com");
-        loginPage.assertThatErrorIs("Please enter your email!");
-    }
-
-    @Test
-    public void noCredentialsLoginTest() {
-        openUrl();
-        loginPage.doLogin("", "");
-        loginPage.assertThatErrorIs("Please enter your email!");
-    }
 
     @Test
     public void successLoginTest() {
